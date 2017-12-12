7da1afbf69d3da3eca317d00c649ff0c0452948f
Esteban Castro <box.esteban@gmail.com>
2017-12-12 16:53:46 -0600

    first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+## Requirements
+* [Ansible 2+](https://www.ansible.com/)
+* [Vagrant 2+](https://www.vagrantup.com/)
+* Vagrant Plugins:
+  * vagrant-vbguest
+* [VirtualBox 5+](https://www.virtualbox.org/)
+
+## Install
+rename example_env to .env
+why /srv ? http://www.codeghar.com/blog/should-web-apps-go-in-srv-or-var-www.html
+run /vagrant/development_build.sh
---
+++ vagrant/development_build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+source ../.env
+
+# rm -rf ./vmwordpress/*
+
+vagrant up
+
+# http://docs.ansible.com/ansible/latest/intro_getting_started.html#host-key-checking
+export ANSIBLE_HOST_KEY_CHECKING=False
+ansible-playbook --user=vagrant --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/new_wordpress_setup.yml --extra-vars "site_name=$1 wordpress_version=$2 edit=True"
+
+sudo ssh -p 2222 -gNfL 80:localhost:80 vagrant@localhost -i .vagrant/machines/default/virtualbox/private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no

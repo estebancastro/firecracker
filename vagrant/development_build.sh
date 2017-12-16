1d848ead7f0e490af198be761279b75aca8750ef
Esteban Castro <box.esteban@gmail.com>
2017-12-15 18:54:34 -0600

    add ansible swap role

--- vagrant/development_build.sh
+++ vagrant/development_build.sh
@@ -8,6 +8,6 @@ vagrant up
 
 # http://docs.ansible.com/ansible/latest/intro_getting_started.html#host-key-checking
 export ANSIBLE_HOST_KEY_CHECKING=False
-ansible-playbook --user=vagrant --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/new_wordpress_setup.yml --extra-vars "site_name=$1 wordpress_version=$2 edit=True"
+ansible-playbook --user=vagrant --inventory-file=./.vagrant/provisioners/ansible/inventory/vagrant_ansible_inventory ../ansible/new_craftcms_setup.yml --extra-vars "site_name=$1 wordpress_version=$2 edit=True"
 
 sudo ssh -p 2222 -gNfL 80:localhost:80 vagrant@localhost -i .vagrant/machines/default/virtualbox/private_key -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no
